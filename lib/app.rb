53631a12eaa54ff0216c15705378a7062017ea23
Kai Ramuenke <kai.ramuenke@gmail.com>
2014-03-09 16:39:48 +1100

    reorganised routes as modules

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,18 @@
-require 'sinatra'
-require 'erb'
-
-class HashTagTrader < Sinatra::Base
-  enable :sessions
-
-end
+require 'sinatra/base'
+require 'sinatra/twitter-bootstrap'
+require 'haml'
 
 require_relative 'routes/routes'
+
+class HashTagTraderApp < Sinatra::Base
+  set :root, File.dirname(__FILE__)
+
+  enable :sessions
+  enable :inline_templates
+
+  register Sinatra::Twitter::Bootstrap::Assets
+  
+  register HashTagTrader::Routes::Index
+  register HashTagTrader::Routes::TwitterBootstrap
+end
+
--- lib/routes/index.rb
+++ lib/routes/index.rb
@@ -1,6 +1,11 @@
-
-class HashTagTrader < Sinatra::Base
-  get '/' do
-    'Welcome to HashTagTrader'
+module HashTagTrader
+  module Routes
+    module Index
+      def self.registered(app)
+        app.get '/' do
+          'Welcome to HashTagTrader'
+        end
+      end
+    end
   end
 end
